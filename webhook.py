7e57b1d10cf5443a568c04c4ff2e26734b58391f
synnek1337 <synnek1337@protonmail.com>
2019-04-04 21:30:28 +0200

    Bug fixes

--- webhook.py
+++ webhook.py
@@ -6,6 +6,7 @@ from os import truncate
 
 class Config():
     def __init__(self):
+        # Discord related stuff below
         self.name = "Example webhook"
         self.channel_id = "123456789012345678"
         self.token = "2944923849238372913921849758973128973218957893210749802315214-123456"
@@ -13,7 +14,7 @@ class Config():
         self.guild_id = "482939203815724551"
         self._id = "489205812349512432"
         self.webhook_url = ""
-        # RSS related stuff be  low
+        # RSS related stuff below
         self.feed_url = "https://www.archlinux.org/feeds/packages/"
 
 
@@ -28,15 +29,23 @@ async def send_news_via_discord(session, url, data):
 
 
 async def main():
-    last_package = open("last_package.txt", "w+")
+    last_package = open("last_package.txt", "r+")
     config = Config()
     async with aiohttp.ClientSession() as session:
         feed = await get_feed_from_rss(session, config.feed_url)
         feed = feed["rss"]["channel"]
         for news in feed["item"]:
-            if news["title"] != last_package.read():
+            last_package.seek(0)
+            if news["title"] in last_package.readline():
+                last_package.seek(0)
+                last_package.truncate()
+                last_package.write(feed["item"][0]["title"])
+                last_package.close()
+                break
+
+            else:
                 data = {
-                        "content": news["title"],
+                        "content": ("New update: " + news["title"]),
                         "name": config.name,
                         "avatar": config.avatar,
                         "channel_id": config.channel_id,
@@ -44,12 +53,8 @@ async def main():
                         "id": config._id,
                         "token": config.token
                         }
-                last_package = open("last_package.txt", "w+")
-                last_package.write(news["title"])
-                last_package.truncate()
                 await send_news_via_discord(session, config.webhook_url, data)
-            else:
-                break
+                
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())

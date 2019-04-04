78f8657cfd168cb81ae51d99022ca8a5fb8abdd9
synnek1337 <synnek1337@protonmail.com>
2019-04-04 19:32:32 +0200

    first release

---
+++ webhook.py
@@ -0,0 +1,51 @@
+import asyncio
+import aiohttp
+import xmltodict
+
+
+class Config():
+    def __init__(self):
+        # Discord related stuff below
+        self.name = "Example webhook"
+        self.channel_id = "123456789012345678"
+        self.token = "2944923849238372913921849758973128973218957893210749802315214-123456"
+        self.avatar = "4929394048fd93944929394048fd9394"
+        self.guild_id = "482939203815724551"
+        self._id = "489205812349512432"
+        self.webhook_url = ""
+        # RSS related stuff be  low
+        self.feed_url = "https://www.archlinux.org/feeds/packages/"
+
+
+async def get_feed_from_rss(session, url):
+    async with session.get(url) as rss_response:
+        feed = await rss_response.text()
+        return xmltodict.parse(feed, process_namespaces=True)
+
+
+async def send_news_via_discord(session, url, data):
+    await session.post(url, json=data)
+
+
+async def main():
+    last_package = open("last_package.txt", "w+")
+    config = Config()
+    async with aiohttp.ClientSession() as session:
+        feed = await get_feed_from_rss(session, config.feed_url)
+        feed = feed["rss"]["channel"]
+        if feed["item"][0]["title"] != last_package.read():
+            data = {
+                    "content": feed["item"][0]["title"],
+                    "name": config.name,
+                    "avatar": config.avatar,
+                    "channel_id": config.channel_id,
+                    "guild_id": config.guild_id,
+                    "id": config._id,
+                    "token": config.token
+                }
+
+            last_package.write(feed["item"][0]["title"])
+            await send_news_via_discord(session, config.webhook_url, data)
+        
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
